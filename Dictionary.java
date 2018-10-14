08a18ebfd656097a1469ac268a20e30856106744
Quang Huy <nqhb00@gmail.com>
2018-10-14 16:20:50 +0000

    Từ Điển

---
+++ Dictionary.java
@@ -0,0 +1,18 @@
+/*
+ * Tu Dien Giao Dien Do Hoa
+ * @author Nguyen Quang Huy
+ * @version 1.0
+ * @since 14-10-2018
+ */
+package dictionary;
+
+public class Dictionary extends Word{
+	/*
+	 * Hàm Dictionary thừa kế hàm Word 
+	 */
+	public Dictionary(String wtarget, String wexplain) {
+		super(wtarget, wexplain);
+	}
+	public Dictionary() {}
+}
+
